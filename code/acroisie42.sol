1b9440977fe4deea96df005c1ffca87b2fa9603a
acroisie <acroisie@student.42kyon.fr>
2025-02-19 14:11:35 +0100

    Refactor Acroisie42 contract: update ownership import, enhance minting and burning functions, and add treasury management features

--- code/acroisie42.sol
+++ code/acroisie42.sol
@@ -1,26 +1,33 @@
-// contracts/GLDToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
-
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Acroisie42 is ERC20, Ownable {
-    constructor() ERC20("acroisie42", "ACR42") {
-        uint256 startAmount = 8 * 10 ** decimals();
+    constructor() ERC20("Acroisie42", "ACR42") Ownable(msg.sender) {
+        uint256 startAmount = 8000 * 10 ** decimals();
         _mint(address(this), startAmount);
     }
 
-    function burn(address account, uint256 burnAmount) external  {
-        _burn(account, burnAmount);
+    function burnTokens(address account, uint256 amount) external {
+        _burn(account, amount);
     }
 
-    function mint(uint256 mintAmount) external  {
-        _mint(address(this), mintAmount);
+    function mintTokensToTreasury(uint256 amount) external onlyOwner {
+        _mint(address(this), amount);
     }
 
-    function getBalance() external view returns(uint256) {
-        return  balanceOf(address(this));
+    function myBalance() external view returns (uint256) {
+        return balanceOf(msg.sender);
+    }
+
+    function transferFromTreasury(address to, uint256 amount) external onlyOwner {
+        require(balanceOf(address(this)) >= amount, "Insufficient funds in treasury.");
+        _transfer(address(this), to, amount);
+    }
+
+    function treasuryBalance() external view returns (uint256) {
+        return balanceOf(address(this));
     }
 }

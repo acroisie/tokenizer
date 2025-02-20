9522f6602ed0dedf7ddeb931ebd9d453d6a239d5
acroisie <acroisie@student.42kyon.fr>
2025-02-20 09:40:41 +0100

    Refactor Acroisie42 contract: remove Ownable inheritance, enhance transaction handling with new enum and event parameters

--- code/acroisie42bonus.sol
+++ code/acroisie42bonus.sol
@@ -2,9 +2,8 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Acroisie42 is ERC20, Ownable {
+contract Acroisie42 is ERC20 {
     uint256 public requiredApprovals;
     address[] public signers;
     mapping(address => bool) public isSigner;
@@ -12,17 +11,20 @@ contract Acroisie42 is ERC20, Ownable {
     mapping(uint256 => mapping(address => bool)) public approvals;
     uint256 public transactionCount;
 
+    enum TransactionType { TRANSFER, MINT, BURN }
+
     struct Transaction {
         address to;
         uint256 amount;
         bool executed;
         uint256 approvalCount;
+        TransactionType txType;
     }
 
-    event TransactionProposed(uint256 indexed txId, address indexed to, uint256 amount);
+    event TransactionProposed(uint256 indexed txId, address indexed to, uint256 amount, TransactionType txType);
     event TransactionApproved(uint256 indexed txId, address indexed signer);
-    event TransactionExecuted(uint256 indexed txId, address indexed to, uint256 amount);
-    event log(address signer);
+    event TransactionExecuted(uint256 indexed txId, address indexed to, uint256 amount, TransactionType txType);
+    event LogSignerAdded(address indexed signer);
 
     modifier onlySigner() {
         require(isSigner[msg.sender], "Not an authorized signer");
@@ -30,14 +32,13 @@ contract Acroisie42 is ERC20, Ownable {
     }
 
     constructor(address[] memory _signers, uint256 _requiredApprovals)
-        ERC20("Acroisie42", "ACR42")
-        Ownable(msg.sender)
+        ERC20("Acroisie42b", "ACR42b")
     {
         require(_signers.length >= _requiredApprovals, "Not enough signers");
 
         for (uint256 i = 0; i < _signers.length; i++) {
             isSigner[_signers[i]] = true;
-            emit log(_signers[i]);
+            emit LogSignerAdded(_signers[i]);
         }
         signers = _signers;
         requiredApprovals = _requiredApprovals;
@@ -46,33 +47,24 @@ contract Acroisie42 is ERC20, Ownable {
         _mint(address(this), startAmount);
     }
 
-    function burnTokens(address account, uint256 amount) external {
-        _burn(account, amount);
-    }
-
-    function mintTokensToTreasury(uint256 amount) external onlyOwner {
-        _mint(address(this), amount);
-    }
-
-    function myBalance() external view returns (uint256) {
-        return balanceOf(msg.sender);
-    }
-
-    function proposeTransferFromTreasury(address to, uint256 amount) external onlySigner {
-        require(balanceOf(address(this)) >= amount, "Insufficient funds in treasury.");
+    function proposeTransaction(address to, uint256 amount, TransactionType txType) external onlySigner {
+        if (txType == TransactionType.TRANSFER) {
+            require(balanceOf(address(this)) >= amount, "Insufficient funds in treasury.");
+        }
 
         uint256 txId = transactionCount++;
         transactions[txId] = Transaction({
             to: to,
             amount: amount,
             executed: false,
-            approvalCount: 0
+            approvalCount: 0,
+            txType: txType
         });
 
-        emit TransactionProposed(txId, to, amount);
+        emit TransactionProposed(txId, to, amount, txType);
     }
 
-    function approveTransfer(uint256 txId) external onlySigner {
+    function approveTransaction(uint256 txId) external onlySigner {
         require(transactions[txId].amount > 0, "Transaction does not exist.");
         require(!transactions[txId].executed, "Transaction already executed.");
         require(!approvals[txId][msg.sender], "Signer already approved.");
@@ -83,20 +75,26 @@ contract Acroisie42 is ERC20, Ownable {
         emit TransactionApproved(txId, msg.sender);
 
         if (transactions[txId].approvalCount >= requiredApprovals) {
-            executeTransfer(txId);
+            executeTransaction(txId);
         }
     }
 
-
-    function executeTransfer(uint256 txId) internal {
+    function executeTransaction(uint256 txId) internal {
         Transaction storage txn = transactions[txId];
         require(!txn.executed, "Transaction already executed.");
         require(txn.approvalCount >= requiredApprovals, "Not enough approvals.");
 
         txn.executed = true;
-        _transfer(address(this), txn.to, txn.amount);
 
-        emit TransactionExecuted(txId, txn.to, txn.amount);
+        if (txn.txType == TransactionType.TRANSFER) {
+            _transfer(address(this), txn.to, txn.amount);
+        } else if (txn.txType == TransactionType.MINT) {
+            _mint(txn.to, txn.amount);
+        } else if (txn.txType == TransactionType.BURN) {
+            _burn(txn.to, txn.amount);
+        }
+
+        emit TransactionExecuted(txId, txn.to, txn.amount, txn.txType);
     }
 
     function treasuryBalance() external view returns (uint256) {
